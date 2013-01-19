88d700514ce8cc2d50e2fbe3b4305ec6d1b658e6
Riki Fridrich <riki@fczbkk.com>
2013-01-19 08:12:23 +0100

    Page refresh after the options are saved.

--- popup.js
+++ popup.js
@@ -20,11 +20,23 @@ function getCheckedRadioId(name) {
     }
 }
 
+function refreshTabs() {
+    var matchPattern = '*://www.pogdesign.co.uk/cat/';
+    
+    // get all tabs with URL matching the pattern and reload them
+    chrome.tabs.query({url : matchPattern}, function (foundTabs) {
+        for (var i = 0, j = foundTabs.length; i < j; i++) {
+            chrome.tabs.reload(foundTabs[i].id);
+        }
+    });
+}
+
 updateRadio();
 
 var typeSubmit = document.getElementById('submitType');
 typeSubmit.addEventListener('click', function() {
     var type = getCheckedRadioId('linkType');
     setOptions(type);
+    refreshTabs();
     window.close();
 });
